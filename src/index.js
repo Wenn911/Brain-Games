8d8c989de339189a34c3f17edf13df02fef36af0
Rostislav Kuznetsov <wenn123@yandex.ru>
2022-01-22 20:37:01 +0300

    add "!" in index.js

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const engine = (gameRules, gamecheck) => {
 
     if (userAnswer !== correctAnswer) {
       console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${correctAnswer}`);
-      console.log(`Let's try again, ${userName}`);
+      console.log(`Let's try again, ${userName}!`);
       return;
     }
     console.log('Correct!');
